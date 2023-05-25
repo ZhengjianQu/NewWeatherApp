5752c9e3ac65c8bcddd594715e39f418844d0665
ZhengjianQu <1528407679@qq.com>
2023-05-25 17:55:43 +0200

    weather page layout, searchbar function is still missing

--- lib/weather.dart
+++ lib/weather.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 import 'package:go_router/go_router.dart';
 
 String apikey = '8dab3e149329990d1d9456befa085601';
+Color hexColor = const Color.fromRGBO(0x62, 0x0, 0xEE, 1.0);
 
 class WeatherData {
   final int temperature;
@@ -64,7 +65,6 @@ class WeatherAppState extends State<WeatherApp> {
   String backgroundImage = ''; // Background image path or color code
 
   void updateBackgroundBasedOnWeatherData(weatherCondition) {
-    print(weatherCondition);
     if (weatherCondition == 'sunny') {
       backgroundImage = 'assets/images/sunny_background.jpg';
     } else if (weatherCondition == 'cloudy') {
@@ -118,12 +118,27 @@ class WeatherAppState extends State<WeatherApp> {
       home: Directionality(
         textDirection: TextDirection.ltr,
         child: Scaffold(
+          backgroundColor: hexColor,
           appBar: AppBar(
             backgroundColor: Colors.black,
-            title: Text('Weather App'),
+            title: SizedBox(
+              height: kToolbarHeight - 10,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'Enter the city name',
+                  hintStyle: const TextStyle(color: Colors.black),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
             actions: [
               IconButton(
-                icon: Icon(Icons.refresh),
+                icon: const Icon(Icons.refresh),
                 onPressed: () async {
                   try {
                     final position = await Geolocator.getCurrentPosition(
@@ -148,62 +163,89 @@ class WeatherAppState extends State<WeatherApp> {
             ),
             child: Center(
                 child: _weatherData != null
-                    ? Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '${_weatherData!.name}',
-                            style: TextStyle(
-                                fontSize: 36, fontWeight: FontWeight.bold),
-                          ),
-                          Image.network(_weatherData!.iconUrl),
-                          SizedBox(height: 16),
-                          Text(
-                            '${_weatherData!.temperature}°C',
-                            style: TextStyle(
-                                fontSize: 48, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            '${_weatherData!.description}',
-                            style: TextStyle(fontSize: 24),
-                          ),
-                          SizedBox(height: 16),
-                          Text(
-                            'Feels like ${_weatherData!.feelsLike}°C',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Text(
-                            'Min: ${_weatherData!.minTemperature}°C / Max: ${_weatherData!.maxTemperature}°C',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Text(
-                            'Pressure: ${_weatherData!.pressure} hPa / Humidity: ${_weatherData!.humidity}%',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Container(
-                            height: 50,
-                            alignment: Alignment.bottomCenter,
-                            child: ElevatedButton(
-                              style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    Color(0xFF000000)),
-                                shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(16.0),
-                                  ),
-                                ),
-                              ),
-                              onPressed: () => context.go('/forecast'),
-                              child: const Text('Forecast'),
+                    ? Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 30), // 设置左右和上下的间距
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10), // 设置圆角半径为10
+                          color: Colors.white.withOpacity(0.5), // 设置半透明的黑色背景
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              _weatherData!.name,
+                              style: const TextStyle(
+                                  fontSize: 36, fontWeight: FontWeight.bold),
                             ),
-                          ),
-                        ],
-                      )
+                            const SizedBox(height: 8),
+                            Text(
+                              '${_weatherData!.temperature}°C',
+                              style: const TextStyle(
+                                  fontSize: 48, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 8),
+                            Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.network(
+                                    _weatherData!.iconUrl,
+                                    width: 50,
+                                    height: 50,
+                                  ),
+                                  const SizedBox(width: 16),
+                                  Text(
+                                    _weatherData!.description,
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              'Feels like ${_weatherData!.feelsLike}°C',
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              'Min: ${_weatherData!.minTemperature}°C / Max: ${_weatherData!.maxTemperature}°C',
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              'Pressure: ${_weatherData!.pressure} hPa / Humidity: ${_weatherData!.humidity}%',
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ))
                     : Text(
                         _errorMessage,
-                        style: TextStyle(fontSize: 24),
+                        style: const TextStyle(fontSize: 24),
                       )),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            color: Colors.black,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => context.go('/forecast'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Forecast',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
